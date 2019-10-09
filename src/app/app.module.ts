c28998682c95eab52a9100039d28f5d57921cd85
cwhaught <cwhaught@gmail.com>
2019-10-08 23:50:20 -0400

    Clean up app styles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import {TodosListComponent} from './todos/list/todos-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatProgressSpinnerModule, MatTableModule} from '@angular/material';
+import {MatMenuModule, MatProgressSpinnerModule, MatTableModule, MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -18,8 +18,10 @@ import {MatProgressSpinnerModule, MatTableModule} from '@angular/material';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    MatMenuModule,
     MatProgressSpinnerModule,
-    MatTableModule
+    MatTableModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
